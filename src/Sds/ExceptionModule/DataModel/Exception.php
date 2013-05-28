ca2b2de8d937a9b46f793dff4895a8e011b04abc
Tim Roediger <superdweebie@gmail.com>
2013-05-28 16:33:22 +1000

    Remove dojo annotation

--- src/Sds/ExceptionModule/DataModel/Exception.php
+++ src/Sds/ExceptionModule/DataModel/Exception.php
@@ -13,7 +13,6 @@ use Sds\DoctrineExtensions\Annotation\Annotations as Sds;
  * @ODM\Document
  * @Sds\Permission\Basic(roles="all", allow="create")
  * @Sds\Permission\Basic(roles="admin", allow={"read", "delete"})
- * @Sds\Dojo\JsonRest
  */
 class Exception {
 
